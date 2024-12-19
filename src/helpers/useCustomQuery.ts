7ffb5aee832dc27f7708c94ad816947295fed916
Pavel Kochetkov <kochetkov_p@mail.ru>
2024-12-20 00:28:28 +0300

    temporarily commented out custom useQuery

--- src/helpers/useCustomQuery.ts
+++ src/helpers/useCustomQuery.ts
@@ -1,14 +1,14 @@
-import { useQuery } from "@tanstack/react-query";
-import { useUsersStore } from "../store/usersStore";
+// import { useQuery } from "@tanstack/react-query";
+// import { useUsersStore } from "../store/usersStore";
 
-export const useCustomQuery = (values: { nickname: string}) => {
-  const { getUsersData } = useUsersStore();  
-  const query = useQuery({
-    queryKey: ['users', values],
-    queryFn: () => getUsersData(values),
-  });
+// export const useCustomQuery = (values: { nickname: string}, page: number, per_page: number) => {
+//   const { getUsersData } = useUsersStore();  
+//   const query = useQuery({
+//     queryKey: ['users', values],
+//     queryFn: () => getUsersData(values, page, per_page),
+//   });
 
-  console.log(query);
+//   console.log(query);
 
-  return query;
-};
+//   return query;
+// };
